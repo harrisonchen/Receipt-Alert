86fe3c6aa9be4a9242d0bdbcf5766764bf3e847e
Harrison Chen <harrisonxchen@gmail.com>
2014-04-08 16:46:37 -0700

    Basic correct rotation of receipt photo

--- ReceiptAlert/src/main/java/com/studentglue/receiptalert/NewReceiptActivity.java
+++ ReceiptAlert/src/main/java/com/studentglue/receiptalert/NewReceiptActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
@@ -59,6 +61,14 @@ public class NewReceiptActivity extends ActionBarActivity {
 
         receipt_imageview.setImageBitmap(receipt_bt);
 
+        //Temporary ... requires API > 11
+        receipt_imageview.setRotation(rotateImageBy(absolutePath));
+
+        /*Matrix matrix = new Matrix();
+        receipt_imageview.setScaleType(ImageView.ScaleType.MATRIX);
+        matrix.postRotate((float) rotateImageBy(absolutePath), 100, 100);
+        receipt_imageview.setImageMatrix(matrix);*/
+
         //receipt_imageview.setImageURI(receipt_uri);
 
         /*if (savedInstanceState == null) {
@@ -66,6 +76,34 @@ public class NewReceiptActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }*/
+    }
+
+    public int rotateImageBy(String absolutePath) {
+
+        int rotate = 0;
+
+        try {
+            ExifInterface exif = new ExifInterface(absolutePath);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+            switch (orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return rotate;
     }
 
     public void saveReceipt(View view) {
